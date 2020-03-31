a5ffacc47bd98f805ab3b629a4943a76a6f5d6eb
Andrea Hayes <amock516@gmail.com>
2020-03-31 13:17:32 -0700

    Initial Commit

---
+++ Ch2Num6AnnualPay/Ch2Num6AnnualPay.cpp
@@ -0,0 +1,25 @@
+/************************************************
+** Author: Andrea Hayes
+** Date: Febuary 20th, 2020
+** Purpose: Figure Annual Pay
+** Input: Pay Amount and Pay Periods
+** Processing: Multilply Pay Amount by Pay Periods to get Annual Pay
+** Output: Display Annual Pay
+*************************************************/
+
+#include <iostream>
+using namespace std;
+int main() {
+    
+    int payAmount = 2200; // Amount earned per pay period
+
+    int payPeriod = 26; // Number of pay periods per year
+
+    int annualPay; // Total annual pay
+
+    annualPay = payAmount * payPeriod;
+
+    cout << "Annual Pay $" << annualPay << endl;
+
+    return 0;
+}
